5fca18f9993a13b20e5941ac380bc5f6fb3ce1c9
Christian Baumann <k.c.baumann@t-online.de>
2026-08-28 21:19:54 +0200

    Multimap: ValueSet Komprimierer: RecordSet SubfieldUtils: $u ist relevant

--- src/de/dnb/basics/collections/Multimap.java
+++ src/de/dnb/basics/collections/Multimap.java
@@ -7,6 +7,7 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
@@ -320,6 +321,14 @@ public abstract class Multimap<K, V> implements Iterable<K>, Serializable, IMult
 			c += getNullSafe(key).size();
 		}
 		return c;
+	}
+
+	public Set<V> getValueSet() {
+		final Set<V> values = new HashSet<>();
+		for (final K key : getKeySet()) {
+			values.addAll(get(key));
+		}
+		return values;
 	}
 
 	@Override
--- src/de/dnb/gnd/utils/Komprimierer.java
+++ src/de/dnb/gnd/utils/Komprimierer.java
@@ -5,6 +5,7 @@ package de.dnb.gnd.utils;
 
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Collection;
 import java.util.Objects;
 
 import de.dnb.basics.Constants;
@@ -20,64 +21,66 @@ import de.dnb.gnd.parser.Record;
  */
 public class Komprimierer extends DownloadWorker {
 
-  /**
-   * @throws IOException
-   *
-   */
-  private Komprimierer() {
-    super();
-  }
+	/**
+	 * @throws IOException
+	 *
+	 */
+	private Komprimierer() {
+		super();
+	}
 
-  /**
-   *
-   * @param inputFolder
-   *            Ordner der Download-Dateien
-   * @param filePrefix
-   *            gemeinsames Präfix aller Download-Dateien
-   * @param gzipFileName
-   *            Zu beschreibende Datei im selben Ordner
-   * @throws IOException
-   *             Wenn die zu lesenden Dateien nicht existieren
-   */
-  public static
-    void
-    komprimiere(final String inputFolder, final String filePrefix, final String gzipFileName)
-      throws IOException {
-    final Komprimierer komprimierer = new Komprimierer();
-    komprimierer.setInputFolder(inputFolder);
-    komprimierer.setFilePrefix(filePrefix);
-    komprimierer.out = MyFileUtils.getGZipPrintStream(gzipFileName);
-    komprimierer.processAllFiles();
-    MyFileUtils.safeClose(komprimierer.out);
+	/**
+	 *
+	 * @param inputFolder  Ordner der Download-Dateien
+	 * @param filePrefix   gemeinsames Präfix aller Download-Dateien
+	 * @param gzipFileName Zu beschreibende Datei im selben Ordner
+	 * @throws IOException Wenn die zu lesenden Dateien nicht existieren
+	 */
+	public static void komprimiere(final String inputFolder, final String filePrefix, final String gzipFileName)
+			throws IOException {
+		final Komprimierer komprimierer = new Komprimierer();
+		komprimierer.setInputFolder(inputFolder);
+		komprimierer.setFilePrefix(filePrefix);
+		komprimierer.out = MyFileUtils.getGZipPrintStream(gzipFileName);
+		komprimierer.processAllFiles();
+		MyFileUtils.safeClose(komprimierer.out);
+	}
 
-  }
+	private PrintStream out;
 
-  private PrintStream out;
+	@Override
+	protected void processRecord(final Record record) {
+		final String recS = toGZip(record);
+		out.println(recS);
+		System.err.println(recS);
+	}
 
-  @Override
-  protected void processRecord(final Record record) {
-    final String recS = toGZip(record);
-    out.println(recS);
-    System.err.println(recS);
-  }
+	/**
+	 * @param record nicht null
+	 * @return Datensatz im GZip-Format, Unicode-Composition!
+	 */
+	public static String toGZip(final Record record) {
+		Objects.requireNonNull(record);
+		return RecordUtils.toPica(record, Format.PICA_PLUS, true, Constants.RS, Constants.MARC_SUB_SEP);
+	}
 
-  /**
-   * @param record  nicht null
-   * @return        Datensatz im GZip-Format, Unicode-Composition!
-   */
-  public static String toGZip(final Record record) {
-    Objects.requireNonNull(record);
-    return RecordUtils.toPica(record, Format.PICA_PLUS, true, Constants.RS, Constants.MARC_SUB_SEP);
-  }
+	public static void toGZip(final Collection<Record> records, final String gzipFileName) throws IOException {
+		final PrintStream outFile = MyFileUtils.getGZipPrintStream(gzipFileName);
+		records.forEach(record -> {
+			final String recS = toGZip(record);
+			outFile.print(recS + Constants.RS + Constants.LF);
+		});
+		MyFileUtils.safeClose(outFile);
+	}
 
-  /**
-   * @param args
-   * @throws IOException
-   */
-  public static void main(final String[] args) throws IOException {
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(final String[] args) throws IOException {
 
-    komprimiere("D:/Analysen/baumann/Musik", "sag.txt", "D:/Analysen/baumann/Musik/sag.gzip");
+		komprimiere("D:/Analysen/baumann/Musik", "sag.txt", "D:/Analysen/baumann/Musik/sag.gzip");
 
-  }
+	}
 
 }
